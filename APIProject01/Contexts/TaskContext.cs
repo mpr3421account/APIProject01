142b07009b67774aaa2d6359cdbe779aecf55d45
Marcos Ribeiro <marcos@azacessorios.com.br>
2023-04-05 10:59:24 -0300

    Context

---
+++ APIProject01/Contexts/TaskContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using APIProject01.Model;
+
+namespace APIProject01.Contexts
+{
+    public class TaskContext : DbContext
+    {
+        public TaskContext(DbContextOptions<TaskContext> options) : base(options)
+        {
+
+        }
+        public DbSet<TaskItem> Tasks { get; set; } = null;
+    }
+}
