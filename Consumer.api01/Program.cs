5cae060e2f66d2d0274d829efdaf03446c84c96d
Marcos Ribeiro <marcos@azacessorios.com.br>
2023-04-06 14:23:05 -0300

    add Get Request

--- Consumer.api01/Program.cs
+++ Consumer.api01/Program.cs
@@ -5,7 +5,7 @@ using Flurl.Http;
 
 Console.WriteLine("Hello, World!");
 
-string url = "https://localhost:7243";
+string url = "https://localhost:44390/";
 
 Item tarefa1 = new Item();
 
@@ -16,18 +16,34 @@ tarefa1.Finished = true;
 Item tarefa2 = new Item();
 
 tarefa2.Id = 2;
-tarefa2.Name = "Pagar Conta";
+tarefa2.Name = "Negar conta";
 tarefa2.Finished = true;
+
+Item tarefa3 = new Item();
+
+tarefa3.Id = 3;
+tarefa3.Name = "Receber DinDin";
+tarefa3.Finished = false;
 
 
 //add a task by POST request
 string endpoint = url + "api/TaskItems";
 
+//slow down the request, waiting for API starts
+//Thread.Sleep(new TimeSpan(0,0,30));
+
 //flur
 endpoint.PostJsonAsync(tarefa1);
 endpoint.PostJsonAsync(tarefa2);
+endpoint.PostJsonAsync(tarefa3);
 
 //read the tasks list by GET request
+IEnumerable<Item> listTasks = await endpoint.GetJsonAsync<IEnumerable<Item>>();
+
+foreach(var item in listTasks)
+{
+    Console.WriteLine($"The task {item.Name} is {item.Finished}");
+}
 
 //change by PUT request
 
