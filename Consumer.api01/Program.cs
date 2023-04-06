d8a65679057033d0cabcfed3884a421106c5df95
Marcos Ribeiro <marcos@azacessorios.com.br>
2023-04-06 11:29:02 -0300

    add endpoint

--- Consumer.api01/Program.cs
+++ Consumer.api01/Program.cs
@@ -1,16 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Consumer.api01;
+using Flurl;
+using Flurl.Http;
+
 Console.WriteLine("Hello, World!");
 
+string url = "https://localhost:7243";
 
-//add a task by POST method
+Item tarefa1 = new Item();
 
-//read the tasks list by GET method
+tarefa1.Id = 1;
+tarefa1.Name = "Pagar Conta";
+tarefa1.Finished = true;
 
-//change by PUT method
+Item tarefa2 = new Item();
+
+tarefa2.Id = 2;
+tarefa2.Name = "Pagar Conta";
+tarefa2.Finished = true;
+
+
+//add a task by POST request
+string endpoint = url + "api/TaskItems";
+
+//flur
+endpoint.PostJsonAsync(tarefa1);
+endpoint.PostJsonAsync(tarefa2);
+
+//read the tasks list by GET request
+
+//change by PUT request
 
 //read again
 
-//delete by DELETE method
+//delete by DELETE request
 
 //read one more time
 
