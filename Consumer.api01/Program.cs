87385316b31cf3e3907d415010dc1eb90b1a3f33
Marcos Ribeiro <marcos@azacessorios.com.br>
2023-04-06 10:04:57 -0300

    Consumer APIs

---
+++ Consumer.api01/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+//add a task by POST method
+
+//read the tasks list by GET method
+
+//change by PUT method
+
+//read again
+
+//delete by DELETE method
+
+//read one more time
+
