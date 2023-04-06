7b75f12a357bf54a242362b81fb32994214e2171
Marcos Ribeiro <marcos@azacessorios.com.br>
2023-04-06 14:30:00 -0300

    Add put request

--- Consumer.api01/Program.cs
+++ Consumer.api01/Program.cs
@@ -7,23 +7,23 @@ Console.WriteLine("Hello, World!");
 
 string url = "https://localhost:44390/";
 
-Item tarefa1 = new Item();
+Item task1 = new Item();
 
-tarefa1.Id = 1;
-tarefa1.Name = "Pagar Conta";
-tarefa1.Finished = true;
+task1.Id = 1;
+task1.Name = "Pagar Conta";
+task1.Finished = true;
 
-Item tarefa2 = new Item();
+Item task2 = new Item();
 
-tarefa2.Id = 2;
-tarefa2.Name = "Negar conta";
-tarefa2.Finished = true;
+task2.Id = 2;
+task2.Name = "Negar conta";
+task2.Finished = true;
 
-Item tarefa3 = new Item();
+Item task3 = new Item();
 
-tarefa3.Id = 3;
-tarefa3.Name = "Receber DinDin";
-tarefa3.Finished = false;
+task3.Id = 3;
+task3.Name = "Receber DinDin";
+task3.Finished = false;
 
 
 //add a task by POST request
@@ -33,9 +33,9 @@ string endpoint = url + "api/TaskItems";
 //Thread.Sleep(new TimeSpan(0,0,30));
 
 //flur
-endpoint.PostJsonAsync(tarefa1);
-endpoint.PostJsonAsync(tarefa2);
-endpoint.PostJsonAsync(tarefa3);
+await endpoint.PostJsonAsync(task1);
+await endpoint.PostJsonAsync(task2);
+await endpoint.PostJsonAsync(task3);
 
 //read the tasks list by GET request
 IEnumerable<Item> listTasks = await endpoint.GetJsonAsync<IEnumerable<Item>>();
@@ -44,10 +44,33 @@ foreach(var item in listTasks)
 {
     Console.WriteLine($"The task {item.Name} is {item.Finished}");
 }
+Console.WriteLine("Vamos alterar: ");
 
 //change by PUT request
 
+endpoint = url + "api/TaskItems/5";
+
+task1.Id = 1;
+task1.Name = "Pagar Conta porra nenhuma";
+task1.Finished = false;
+
+task2.Id = 2;
+task2.Name = "Negar Conta é meu sonho";
+task2.Finished = false;
+
+task3.Id = 3;
+task3.Name = "Receber DinDin tá difícil";
+task3.Finished = true;
+
 //read again
+listTasks = await endpoint.GetJsonAsync<IEnumerable<Item>>();
+
+foreach (var item in listTasks)
+{
+    Console.WriteLine($"The task {item.Name} is {item.Finished}");
+}
+Console.WriteLine("Vamos deletar: ");
+
 
 //delete by DELETE request
 
